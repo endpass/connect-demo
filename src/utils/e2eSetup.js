ab4d5b8bba3448ce44c8c5e7c5bd650eb55d8428
idudiq <idudiq@gmail.com>
2019-08-13 20:00:09 +0300

    refactor(e2e): change e2e setup

--- src/utils/e2eSetup.js
+++ src/utils/e2eSetup.js
@@ -1,25 +1,7 @@
-import { SWHost } from '@endpass/e2e-utils';
-
-const rafAsync = () =>
-  new Promise(resolve => {
-    requestAnimationFrame(resolve);
-  });
-const awaitElement = async selector => {
-  while (!document.querySelector(selector)) {
-    // eslint-disable-next-line
-    await rafAsync();
-  }
-
-  return document.querySelector(selector);
-};
+import E2EBridge from '@endpass/e2e-utils/E2EBridge';
 
 const e2eSetup = async bus => {
-  const bridgeFrame = await awaitElement('[data-endpass=frame]');
-  const e2eBridge = new SWHost({
-    mockTarget: bridgeFrame.contentWindow,
-    name: 'e2e-demo',
-  });
-
+  const e2eBridge = new E2EBridge();
   Object.assign(bus, {
     e2eBridge,
   });
