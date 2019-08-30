1fa609c6a8f303210f499e2987b445b71c418e9e
idudiq <idudiq@gmail.com>
2019-08-30 11:44:17 +0300

    fix: setup e2e

--- src/utils/e2eSetup.js
+++ src/utils/e2eSetup.js
@@ -1,6 +1,6 @@
 import E2EBridge from '@endpass/e2e-utils/E2EBridge';
 
-const e2eSetup = async bus => {
+const e2eSetup = bus => {
   const e2eBridge = new E2EBridge();
   Object.assign(bus, {
     e2eBridge,
