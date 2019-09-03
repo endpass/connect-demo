501fa69bbaa0e665b8c802a2da96683dc6dcf3a1
idudiq <idudiq@gmail.com>
2019-09-03 18:43:05 +0300

    chore(e2e): change symlinks for e2e dev mode

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -5,4 +5,5 @@ declare namespace ENV {
   const VUE_APP_OAUTH_SERVER: string;
   const VUE_APP_OAUTH_CLIENT_ID: string;
   const VUE_APP_IS_E2E_CONNECT: boolean;
+  const VUE_APP_WITH_SYMLINKS: boolean;
 }
