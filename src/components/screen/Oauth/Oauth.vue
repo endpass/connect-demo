8286c7c5aa185ed051c8cc1efab2d17199b7aa51
idudiq <idudiq@gmail.com>
2019-11-05 15:32:58 +0300

    style: formatting

--- src/components/screen/Oauth/Oauth.vue
+++ src/components/screen/Oauth/Oauth.vue
@@ -5,7 +5,10 @@
       data-test="endpass-app-loader"
       label="Please wait, oauth usage is loading..."
     />
-    <div v-if="isInited" class="section">
+    <div
+      v-if="isInited"
+      class="section"
+    >
       <div class="card">
         <div class="card-content">
           <div>
@@ -18,7 +21,10 @@
                 >
                   <div class="tags has-addons">
                     <span class="tag">OAuth open mode</span>
-                    <span class="tag" :class="openModeClass">{{
+                    <span
+                      class="tag"
+                      :class="openModeClass"
+                    >{{
                       openModeTitle
                     }}</span>
                   </div>
@@ -26,7 +32,10 @@
               </div>
             </form-field>
             <v-tabs>
-              <v-tab label="Requests" data-test="endpass-oauth-requests-tab">
+              <v-tab
+                label="Requests"
+                data-test="endpass-oauth-requests-tab"
+              >
                 <p class="subtitle">
                   Each button can do request to oauth server with different
                   scopes
