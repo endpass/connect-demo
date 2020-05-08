ada8ddf0d37acd3d21a1b0354e862df57909624f
dudiq <idudiq@gmail.com>
2020-05-08 21:59:35 +0300

    fix: init buttons and styles for them

--- src/components/screen/Oauth/Oauth.vue
+++ src/components/screen/Oauth/Oauth.vue
@@ -1,47 +1,49 @@
 <template>
-  <content-loader :is-loading="isLoading">
-    <span slot="label">Please wait, Oauth usage is loading...</span>
-    <section
-      v-if="isInited"
-      class="oauth-section"
-    >
-      <v-card class="card-content">
-        <form-field>
-          <v-header-controls />
-        </form-field>
-        <form-field>
-          <popup-mode-switcher
-            :value="openMode"
-            :options="$options.modeOptions"
-            @switch="onSwitchOauthPopup"
-          />
-        </form-field>
-      </v-card>
-    </section>
-    <section class="oauth-section">
-      <v-card class="card-content">
-        <div class="oauth-content-header">
-          Here are a few examples of how the "Sign In with Endpass" button would
-          look to a user inside your application
-        </div>
-        <div class="oauth-content-login-cards-list">
-          <login-card label="Default style" />
-          <login-card
-            label="Inverse style"
-            :is-inverted-colors="true"
-          />
-        </div>
-      </v-card>
-    </section>
-    <section class="oauth-section">
-      <v-card class="card-content">
-        <div class="oauth-content-header">
-          Each button can do request to OAuth server
-        </div>
-        <requests :is-loading.sync="isLoadingContent" />
-      </v-card>
-    </section>
-  </content-loader>
+  <div class="oauth-container">
+    <content-loader
+      :is-loading="isLoading"
+      label="Please wait, Oauth usage is loading..."
+    />
+    <div v-if="isInited">
+      <section class="oauth-section">
+        <v-card class="card-content">
+          <form-field>
+            <v-header-controls />
+          </form-field>
+          <form-field>
+            <popup-mode-switcher
+              :value="openMode"
+              :options="$options.modeOptions"
+              @switch="onSwitchOauthPopup"
+            />
+          </form-field>
+        </v-card>
+      </section>
+      <section class="oauth-section">
+        <v-card class="card-content">
+          <div class="oauth-content-header">
+            Here are a few examples of how the "Sign In with Endpass" button
+            would look to a user inside your application
+          </div>
+          <div class="oauth-content-login-cards-list">
+            <login-card label="Default style" />
+            <login-card
+              label="Inverse style"
+              :is-inverted-colors="true"
+            />
+          </div>
+        </v-card>
+      </section>
+      <section class="oauth-section">
+        <v-card class="card-content">
+          <div class="oauth-content-header">
+            Each button can do request to OAuth server
+          </div>
+          <requests :is-loading.sync="isLoadingContent" />
+        </v-card>
+      </section>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -127,7 +129,13 @@ export default {
 </script>
 
 <style lang="scss">
-.oauth-section {
+.oauth-section:not(:last-child) {
+  margin-bottom: 16px;
+}
+.oauth-content-login-cards-list {
+  display: flex;
+}
+.oauth-container {
   margin-bottom: 16px;
 }
 </style>
