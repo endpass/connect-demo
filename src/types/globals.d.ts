ab36e30ec85c9ca615f88a7d26c0cafb6faf6ac7
Raymond <raymondcostner@gmail.com>
2020-06-10 13:11:54 +0300

    fix: remove useless string from env

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -3,7 +3,6 @@ declare namespace ENV {
   const VUE_APP_AUTH_URL: string;
   const VUE_APP_OAUTH_SERVER: string;
   const VUE_APP_OAUTH_CLIENT_ID: string;
-  const VUE_APP_DEMO_SERVER: string;
   const VUE_APP_IS_E2E_CONNECT: boolean;
   const VUE_APP_WITH_SYMLINKS: boolean;
 }
