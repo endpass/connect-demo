872d8454dcb92a7a68e84c65f34d53f9c82d0f4d
Raymond <raymondcostner@gmail.com>
2020-06-22 10:15:54 +0300

    feat: use VUE_APP_IS_STRICT instead production flag

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -1,5 +1,6 @@
 declare namespace ENV {
   const VUE_APP_IS_PRODUCTION: boolean;
+  const VUE_APP_IS_STRICT: boolean;
   const VUE_APP_AUTH_URL: string;
   const VUE_APP_OAUTH_SERVER: string;
   const VUE_APP_OAUTH_CLIENT_ID: string;
